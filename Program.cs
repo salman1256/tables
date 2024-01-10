e9da49b00a3721549851b21733c02a777c92949f
salman1256 <salman1256@yahoo.com>
2024-01-10 12:17:19 +0530

    Adding 7

--- Program.cs
+++ Program.cs
@@ -10,3 +10,10 @@ for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"{num}*{i} = \t {(num * i)}");
 }
+
+Console.WriteLine("Table of 7");
+num = 7;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{num}*{i} = \t {(num * i)}");
+}
